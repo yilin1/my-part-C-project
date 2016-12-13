dde06ec1d5abf7712e04dfc66964d5cb20387087
yilin1 <yilin_shi@hotmail.com>
2016-12-13 11:58:04 +0000

    Update functions for other convergence plots MCMC (b4 wk8 meeting).R

--- functions for other convergence plots MCMC (b4 wk8 meeting).R
+++ functions for other convergence plots MCMC (b4 wk8 meeting).R
@@ -4,7 +4,7 @@ chainplot=function(psdata, erv1=0.1, erv2=0.1, t=50, tby=1, n=1000, m){# this gi
 	chain2=myMCMC(psdata, erv1=erv1, erv2=erv2, t=t, tby=tby, n=n, m=m)
 	chain3=myMCMC(psdata, erv1=erv1, erv2=erv2, t=t, tby=tby, n=n, m=m)
 	chain4=myMCMC(psdata, erv1=erv1, erv2=erv2, t=t, tby=tby, n=n, m=m)
-	plot(chain1,type='l',main=paste('4-chain comparison',',','m=',m),ylab='chains',xlab='iteration')
+	plot(chain1,type='l',main=paste('4-chain comparison',',','m=',m),ylab='chains',xlab='iteration',ylim=c(0,1))
 	lines(chain2,col='red')
 	lines(chain3,col='green')
 	lines(chain4,col='blue')
