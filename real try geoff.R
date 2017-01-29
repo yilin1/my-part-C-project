25c60ceaab1c284eda37b7663e85690299c34ecf
yilin1 <yilin_shi@hotmail.com>
2017-01-29 16:43:59 +0000

    Add files via upload

--- real try geoff.R
+++ real try geoff.R
@@ -45,8 +45,10 @@ realtest27=myMCMC14a_vtest_evk_realtestyesfuben(data=psdata,v=1:14,k=50,n=280000
 realtest28=myMCMC14a_vtest_evk_realtestyesfuben(data=psdata,v=1:14,k=1000,n=2800000,erv1=1,erv2=erv2,tby=1)
 realtest29=myMCMC14a_vtest_evk_realtestyesfuben(data=psdata,v=1:14,k=1000,n=2800000,erv1=1,erv2=erv2,tby=1)
 
+realtest30=myMCMC14a_vtest_evk_realtestyesfuben(data=psdata,v=1:14,k=1000,n=3000000,erv1=1,erv2=erv2,tby=1)
+realtest31=myMCMC14a_vtest_evk_realtestyesfuben(data=psdata,v=1:14,k=1000,n=3000000,erv1=1,erv2=erv2,tby=1)
 sink()
-sink("realtest2829(280)sink")
+sink("realtest3031(300)sink")
 print(erv2)
 
 for (i in 1:14){
