0b64eeb15c0e5514e90481816fbd52dac6cf4670
yilin1 <yilin_shi@hotmail.com>
2017-02-03 16:48:25 +0000

    Add files via upload

--- sigfile functions/real/whichdata function.R
+++ sigfile functions/real/whichdata function.R
@@ -1,9 +1,11 @@
 
-whichdata=function(num){
+whichdata=function(num,home=TRUE){
 	#this function returns the data for the specific patient numbered 'num'
 	#so num ranges from 1 to 69
-
-  rd=dragdata()
+  if (home==FALSE){
+    rd=dragdata(home=FALSE)
+  } else{
+  rd=dragdata()}
 	which1=which(rd$Patient.No.==num)
 	#which2=rd[which1,]
 	#which3=as.numeric(as.vector(which2$ratio)) Q:how to deal with the starred datas?
